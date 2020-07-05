53fcbdd90745eaf7ce695c42ea1023e68a8f2c04
Iry-Isa <iryisa@gmail.com>
2020-07-05 04:24:15 +0200

    estilos

--- src/app/blog/blog.routing.ts
+++ src/app/blog/blog.routing.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
     { path: 'articulos', component: ArticulosComponent },
     { path: 'articulo', component: ArticuloComponent },
     {path: 'detalleArticulo', component: DetalleArticuloComponent },
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    { path: '', redirectTo: '/seccion', pathMatch: 'full' }
 ];
 
 @NgModule({
